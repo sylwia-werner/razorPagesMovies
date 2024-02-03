8a0fbdb3921bd7e22ad9c34b25d6070b8e4ba62e
Sylek <sylwia.werner@oke.pl>
2024-02-03 12:40:36 +0100

    complete db, search and validation part

--- obj/Debug/net8.0/RazorPagesMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/RazorPagesMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdf35c18d1dffd8ba6dfe45a5467bfaf5bfc3d9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
